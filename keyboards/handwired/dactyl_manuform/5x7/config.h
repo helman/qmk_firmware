320dbf7deaeb814fe29edc63aab199eaf54bb6b1
Helman Budianto <helman@heldes.com>
2022-06-07 14:06:08 +1000

    Add helman layout

--- keyboards/handwired/dactyl_manuform/5x7/config.h
+++ keyboards/handwired/dactyl_manuform/5x7/config.h
@@ -20,9 +20,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-#define PRODUCT_ID 0x3537
-#define DEVICE_VER 0x0001
-#define PRODUCT    Dactyl-Manuform (5x7)
+#define PRODUCT_ID      0x3537
+#define DEVICE_VER      0x0001
+#define PRODUCT         Dactyl-Manuform (5x7)
 
 /* key matrix size */
 // Rows are doubled-up
@@ -33,8 +33,132 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROW_PINS { D4, C6, D7, E6, B4, B5 }
 #define MATRIX_COL_PINS { F5, F6, F7, B1, B3, B2, B6 }
 
-#define DIODE_DIRECTION COL2ROW
+#define DIODE_DIRECTION ROW2COL
 
 // WS2812 RGB LED strip input and number of LEDs
 #define RGB_DI_PIN D3
-#define RGBLED_NUM 12
+#define RGBLED_NUM 74
+#define RGBLED_SPLIT { 37, 37 }
+
+#define DRIVER_LED_TOTAL RGBLED_NUM
+
+#ifdef RGB_MATRIX_ENABLE
+
+#   define ENABLE_RGB_MATRIX_ALPHAS_MODS
+#   define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#   define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#   define ENABLE_RGB_MATRIX_BREATHING
+#   define ENABLE_RGB_MATRIX_BAND_SAT
+#   define ENABLE_RGB_MATRIX_BAND_VAL
+#   define ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+#   define ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+#   define ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+#   define ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+#   define ENABLE_RGB_MATRIX_CYCLE_ALL
+#   define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#   define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#   define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#   define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+#   define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+#   define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
+#   define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
+#   define ENABLE_RGB_MATRIX_DUAL_BEACON
+#   define ENABLE_RGB_MATRIX_RAINBOW_BEACON
+#   define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+#   define ENABLE_RGB_MATRIX_RAINDROPS
+#   define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+#   define ENABLE_RGB_MATRIX_HUE_BREATHING
+#   define ENABLE_RGB_MATRIX_HUE_PENDULUM
+#   define ENABLE_RGB_MATRIX_HUE_WAVE
+#   define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+#   define ENABLE_RGB_MATRIX_DIGITAL_RAIN
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+#   define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+#   define ENABLE_RGB_MATRIX_SPLASH
+#   define ENABLE_RGB_MATRIX_MULTISPLASH
+#   define ENABLE_RGB_MATRIX_SOLID_SPLASH
+#   define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+
+#   define RGB_MATRIX_TYPING_HEATMAP_DECREASE_DELAY_MS 50
+
+#   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+//#define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
+#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS // enable framebuffer effects
+#   define RGB_DISABLE_TIMEOUT 0 // number of milliseconds to wait until rgb automatically turns off
+#   define RGB_DISABLE_AFTER_TIMEOUT 0 // OBSOLETE: number of ticks to wait until disabling effects
+#   define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
+#   define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+#   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#   define RGB_MATRIX_MAXIMUM_BRIGHTNESS 100 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
+#   define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CYCLE_LEFT_RIGHT // Sets the default mode, if none has been set
+#   define RGB_MATRIX_STARTUP_HUE 0 // Sets the default hue value, if none has been set
+#   define RGB_MATRIX_STARTUP_SAT 255 // Sets the default saturation value, if none has been set
+#   define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
+#   define RGB_MATRIX_STARTUP_SPD 127 // Sets the default animation speed, if none has been set
+//#define RGB_MATRIX_DISABLE_KEYCODES // disables control of rgb matrix by keycodes (must use code functions to control the feature)
+#   define RGB_MATRIX_SPLIT RGBLED_SPLIT     // (Optional) For split keyboards, the number of LEDs connected on each half. X = left, Y = Right.
+#   define SPLIT_TRANSPORT_MIRROR
+
+#endif
+
+// define all of this if rgb light is enabled
+#ifdef RGBLIGHT_ENABLE
+
+#   define RGBLIGHT_ANIMATIONS
+#   define RGBLIGHT_LIMIT_VAL 100
+#   define RGBLIGHT_HUE_STEP 5
+#   define RGBLIGHT_SAT_STEP 5
+#   define RGBLIGHT_VAL_STEP 5
+#   define RGBLIGHT_SLEEP
+#   define RGBLIGHT_SPLIT
+#   define RGBLIGHT_DEFAULT_MODE 14
+
+// Enable Light Layers implementation
+#   define RGBLIGHT_LAYERS
+// Allow Light Layers to override RGB off configuration
+#   define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
+#   define LED_LAYOUT( \
+  L11, L12, L13, L14, L15, L16, L17,       R11, R12, R13, R14, R15, R16, R17, \
+  L21, L22, L23, L24, L25, L26, L27,       R21, R22, R23, R24, R25, R26, R27, \
+  L31, L32, L33, L34, L35, L36, L37,       R31, R32, R33, R34, R35, R36, R37, \
+  L41, L42, L43, L44, L45, L46,                 R41, R42, R43, R44, R45, R46, \
+  L51, L52, L53, L54,                                     R51, R52, R53, R54, \
+                 L55, L56,                           R55, R56, \
+                      L61, L62,                 R61, R62, \
+                      L63, L64,                 R63, R64 ) \
+  { \
+  L17, L16, L15, L14, L13, L12, L11, \
+  L27, L26, L25, L24, L23, L22, L21, \
+  L37, L36, L35, L34, L33, L32, L31, \
+  L46, L45, L44, L43, L42, L41, \
+  L56, L55, L54, L53, \
+                 L52, L51, \
+                      L62, L61, \
+                      L64, L63, \
+  R17, R16, R15, R14, R13, R12, R11, \
+  R27, R26, R25, R24, R23, R22, R21, \
+  R37, R36, R35, R34, R33, R32, R31, \
+       R46, R45, R44, R43, R42, R41, \
+                 R56, R55, R54, R53, \
+            R52, R51, \
+       R62, R61, \
+       R64, R63 \
+  }
+
+#   define RGBLIGHT_LED_MAP LED_LAYOUT( \
+   6,  5,  4,  3,  2,  1,  0,      13, 12, 11, 10,  9,  8,  7, \
+  21, 22, 23, 24, 25, 26, 27,      14, 15, 16, 17, 18, 19, 20, \
+  34, 33, 32, 31, 30, 29, 28,      41, 40, 39, 38, 37, 36, 35, \
+  48, 49, 50, 51, 52, 53,              42, 43, 44, 45, 46, 47, \
+  59, 58, 57, 56,                              73, 72, 71, 70, \
+              55, 54,                      69, 68, \
+                  62, 61,              67, 64, \
+                  63, 60,              66, 65 )
+
+#endif
--- keyboards/handwired/dactyl_manuform/5x7/keymaps/helman/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/helman/keymap.c
@@ -1,4 +1,6 @@
 #include QMK_KEYBOARD_H
+#include "rgb.h"
+#include "rgb_matrix_types.h"
 
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
@@ -24,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB,        KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_MUTE,
    LT(_FN,KC_ESC),  KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_LBRC,
    KC_LSFT,       KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
-   KC_LCTL,       KC_ESC,   ,  TT(_FN),
+   KC_LCTL,       KC_ESC,   LGUI(LALT(KC_EJCT)),  MO(_FN),
                                KC_BSPC, KC_SPC,
                                KC_LALT, KC_END,
                                KC_LGUI, KC_HOME,
@@ -41,17 +43,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_FN] = LAYOUT_5x7(
   // left hand
    KC_TRNS,   KC_F1,     KC_F2,      KC_F3,    KC_F4,    KC_F5,    KC_F6,
-   _______,   _______,   _______,    KC_UP,    KC_NO,    RGB_SPI,  RGB_VAI,
-   _______,   _______,   KC_LEFT,    KC_DOWN,  KC_RGHT,  RGB_SPD,  RGB_VAD,
-   _______,   _______,   _______,   _______,   KC_NO,    KC_NO,
+   _______,   KC_NO,   KC_NO,    KC_UP,    KC_NO,    RGB_SPI,  RGB_VAI,
+   _______,   KC_NO,   KC_LEFT,    KC_DOWN,  KC_RGHT,  RGB_SPD,  RGB_VAD,
+   _______,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
    KC_MSTP,   KC_MPLY,   KC_MPRV,   KC_MNXT,
-                               RGB_TOG, _______,
-                               _______, _______,
-                               _______, _______,
+                               RGB_TOG, RGB_MOD,
+                               RGB_SAI, RGB_SAD,
+                               RGB_HUI, RGB_HUD,
         // right hand
                      KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_NO,
-                     RGB_MOD,   RGB_RMOD,  RGB_SAI,   RGB_SAD,   RGB_HUI,   RGB_HUD,   KC_NO,
-                     RGB_TOG,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   KC_NO,     KC_NO,
+                     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,
+                     KC_NO,     KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   KC_NO,     KC_NO,
                                 RGB_M_P,   RGB_M_B,   RGB_M_R,   RGB_M_SW,  RGB_M_SN,  KC_NO,
                                                       RGB_M_K,   RGB_M_X,   RGB_M_G,   RGB_M_T,
         KC_DEL, _______,
@@ -98,5 +100,83 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,
         _______, _______),
 
-
 };
+
+#ifdef RGB_MATRIX_ENABLE
+    led_config_t g_led_config = { {
+      // Key Matrix to LED Index
+      {        6,      5,  4,  3,  2,      1,      0 },
+      {       13,     12, 11, 10,  9,      8,      7 },
+      {       20,     19, 18, 17, 16,     15,    14 },
+      {       26,     25, 24, 23, 22,     21, NO_LED },
+      {       32,     31, 30, 29, 28,     27, NO_LED },
+      {   NO_LED, NO_LED, 36, 35, 34,     33, NO_LED },
+      {       43,     42, 41, 40, 39,     38,     37 },
+      {       50,     49, 48, 47, 46,     45,     44 },
+      {       57,     56, 55, 54, 53,     52,     51 },
+      {   NO_LED,     63, 62, 61, 60,     59,     58 },
+      {   NO_LED,     69, 68, 67, 66,     65,     64 },
+      {   NO_LED,     73, 72, 71, 70, NO_LED, NO_LED }
+    }, {
+      // LED Index to Physical Position
+      {   0,   0 }, {  16,   0 }, {  32,   0 }, {  48,   0 }, {  64,   0 }, {  80,   0 }, {  96,   0 },
+      {   0,  16 }, {  16,  16 }, {  32,  16 }, {  48,  16 }, {  64,  16 }, {  80,  16 }, {  96,  16 },
+      {   0,  32 }, {  16,  32 }, {  32,  32 }, {  48,  32 }, {  64,  32 }, {  80,  32 }, {  96,  32 },
+      {   0,  40 }, {  16,  40 }, {  32,  40 }, {  48,  40 }, {  64,  40 }, {  80,  40 },
+      {   0,  48 }, {  16,  48 }, {  32,  48 }, {  48,  48 },        {  72,  48 }, { 104,  48 },
+                                                                            {  80,  56 }, {  96,  56 },
+                                                                            {  80,  64 }, {  96,  64 },
+      { 128,   0 }, { 144,   0 }, { 160,   0 }, { 176,   0 }, { 192,   0 }, { 208,   0 }, { 224,   0 },
+      { 128,  16 }, { 144,  16 }, { 160,  16 }, { 176,  16 }, { 192,  16 }, { 208,  16 }, { 224,  16 },
+      { 128,  32 }, { 144,  32 }, { 160,  32 }, { 176,  32 }, { 192,  32 }, { 208,  32 }, { 224,  32 },
+                    { 144,  40 }, { 160,  40 }, { 176,  40 }, { 192,  40 }, { 208,  40 }, { 224,  40 },
+           { 136,  48 }, { 152,  48 },          { 176,  48 }, { 192,  48 }, { 208,  48 }, { 224,  48 },
+      { 128,  56 }, { 144,  56 },
+      { 128,  64 }, { 144,  64 }
+    }, {
+      // LED Index to Flag
+      1, 4, 4, 4, 4, 4, 1,
+      1, 4, 4, 4, 4, 4, 4,
+      1, 4, 4, 4, 4, 4, 4,
+      1, 4, 4, 4, 4, 4,
+      1, 1, 1, 1, 1, 1,
+               1, 1, 1, 1,
+      1, 4, 4, 4, 4, 4, 1,
+      4, 4, 4, 4, 4, 4, 1,
+      4, 4, 4, 4, 4, 4, 1,
+         4, 4, 4, 4, 4, 1,
+         1, 1, 1, 1, 1, 1,
+      1, 1, 1, 1,
+    } };
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+    // Light LEDs 6 to 9 and 12 to 15 red when caps lock is active. Hard to ignore!
+    const rgblight_segment_t PROGMEM my_fn_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    //    {0, 74, HSV_GOLD},      // Set all keys to gold color
+        {24, 1, HSV_CYAN},      // Light E key to cyan
+        {30, 3, HSV_CYAN},      // Light SDF key to cyan
+        {36, 4, HSV_CYAN}       // Light HJKL key to cyan
+    );
+    // Light LEDs 9 & 10 in cyan when keyboard layer 1 is active
+    const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+        {9, 2, HSV_CYAN}
+    );
+
+    // Now define the array of layers. Later layers take precedence
+    const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+        my_fn_layer // Overrides default layer
+    );
+
+    void keyboard_post_init_user(void) {
+        // Enable the LED layers
+        rgblight_layers = my_rgb_layers;
+    }
+
+    // Enable layer on FN key press
+    layer_state_t layer_state_set_user(layer_state_t state) {
+        rgblight_set_layer_state(0, layer_state_cmp(state, _FN));
+        return state;
+    }
+#endif
+
